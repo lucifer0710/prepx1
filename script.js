5d864f939db8584644591e40e99e5cace5596ffc
Kunal <singlakunal313@gmail.com>
2026-06-11 08:07:46 +0530

    Integrate GPAcalc and timetable under public directory and update relative redirects

--- script.js
+++ script.js
@@ -80,11 +80,11 @@ function toggleCard(header) {
             return match ? match[0] : null;
         }
         function openGPACalculator() {
-           window.open('https://gpacalcxx.vercel.app/' , '_blank');
+           window.open('/GPAcalc/' , '_blank');
         }
 
         function openTimetable() {
-    window.open('https://timetablexx.vercel.app/', '_blank');
+    window.open('/timetable/', '_blank');
 }
 window.toggleCard = toggleCard;
 window.filterContent = filterContent;
